4764a9acb46f08362f3f1fb7ab1f58b02e906fc8
Sanuraj M <developer.sanuraj@gmail.com>
2020-12-11 00:09:23 +0530

    fix

--- toaster_kotlin/src/main/java/com/example/toaster_kotlin/ToasterKotlin.kt
+++ toaster_kotlin/src/main/java/com/example/toaster_kotlin/ToasterKotlin.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 object ToasterKotlin {
-    fun showToast(context: Context, message: String) {
+    @JvmStatic fun showToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
